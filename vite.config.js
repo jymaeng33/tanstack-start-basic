f938f4bad0c567c0d77d64cdd36a80604c80e78b
jymaeng33 <jymaeng@gaonprime.com>
2025-12-24 11:23:37 +0900

    fix: nitro preset

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 
-import viteReact from "@vitejs/plugin-react";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), nitro()],
   nitro: {
-    preset: "standard",
+    preset: "aws_amplify",
   },
 });
